8ef637fe814ab481bc0035b5bbc4081820a12099
RXGabriel <gabriel2005rf@gmail.com>
2024-08-22 11:56:10 -0300

    feat: add sidebar toggle functionality

--- client/src/app/(components)/Navbar.tsx
+++ client/src/app/(components)/Navbar.tsx
@@ -2,12 +2,25 @@
 
 import { Bell, Menu, Settings, Sun } from "lucide-react";
 import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "../redux";
+import { setIsSidebarCollapsed } from "@/state";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSideBarCollapsed));
+  };
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
       <div className="flex justify-between items-center gap-5">
-        <button className="px-3 py-3  bg-gray-100 rounded-full hover:bg-blue-100">
+        <button
+          className="px-3 py-3  bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={toggleSidebar}
+        >
           <Menu className="w-4 h-4" />
         </button>
 
