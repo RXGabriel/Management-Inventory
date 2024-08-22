65aa8fd48e91c494739ac718e9003141268d8fb2
RXGabriel <gabriel2005rf@gmail.com>
2024-08-22 15:31:16 -0300

    feat: add dark mode toggle and update navbar imports

--- client/src/app/(components)/Navbar.tsx
+++ client/src/app/(components)/Navbar.tsx
@@ -1,24 +1,29 @@
-"use client ";
+"use client";
 
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
+import React from "react";
 import Link from "next/link";
-import { useAppDispatch, useAppSelector } from "../redux";
-import { setIsSidebarCollapsed } from "@/state";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
-  const isSideBarCollapsed = useAppSelector(
+  const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed
   );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
   const toggleSidebar = () => {
-    dispatch(setIsSidebarCollapsed(!isSideBarCollapsed));
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
   };
 
   return (
     <div className="flex justify-between items-center w-full mb-7">
       <div className="flex justify-between items-center gap-5">
         <button
-          className="px-3 py-3  bg-gray-100 rounded-full hover:bg-blue-100"
+          className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
           onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
@@ -40,8 +45,12 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button>
-              <Sun className="cursor-pointer text-gray-500" size={24} />
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
             </button>
           </div>
 
