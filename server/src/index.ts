4fab78649e796a0c608ed0b494549c04caa5c9b0
RXGabriel <gabriel2005rf@gmail.com>
2024-08-26 15:30:51 -0300

    feat: add dashboard routes to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,8 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use("/dashboard", dashboardRoutes);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
