e2ef65d9b020bd2e07dae4b00ea314b72e5783bc
RXGabriel <gabriel2005rf@gmail.com>
2024-08-28 16:07:01 -0300

    feat: add CreateProductModal component

---
+++ client/src/app/products/CreateProductModal.tsx
@@ -0,0 +1,135 @@
+import React, {
+  ChangeEvent,
+  ChangeEventHandler,
+  FormEvent,
+  useState,
+} from "react";
+import { v4 } from "uuid";
+import Header from "@/app/(components)/header";
+
+type ProductFormProps = {
+  name: string;
+  price: number;
+  stockQuantity: number;
+  rating: number;
+};
+
+type CreateProductModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onCreate: (formaData: ProductFormProps) => void;
+};
+
+const CreateProductModal = ({
+  isOpen,
+  onClose,
+  onCreate,
+}: CreateProductModalProps) => {
+  const [formData, setFormData] = useState({
+    productId: v4(),
+    name: "",
+    price: 0,
+    stockQuantity: 0,
+    rating: 0,
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onCreate(formData);
+    onClose();
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]:
+        name === "price" || name === "stockQuantity" || name === "rating"
+          ? parseFloat(value)
+          : value,
+    });
+  };
+
+  const inputCssStyles =
+    "block w-full mb-2 p-2 border-gray-500 border-2 rounded-md";
+
+  const labelCssStyles = "block text-sm font-medium text-gray-700";
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20">
+      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+        <Header name="Adicionar um novo produto" />
+        <form onSubmit={handleSubmit} className="mt-5 ">
+          <label htmlFor="productName" className={labelCssStyles}>
+            Nome do Produto
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+            value={formData.name}
+            className={inputCssStyles}
+            required
+          />
+
+          <label htmlFor="productPrice" className={labelCssStyles}>
+            Preço
+          </label>
+          <input
+            type="number"
+            name="number"
+            placeholder="Preço"
+            onChange={handleChange}
+            value={formData.price}
+            className={inputCssStyles}
+            required
+          />
+
+          <label htmlFor="productName" className={labelCssStyles}>
+            Quantidade no estoque
+          </label>
+          <input
+            type="number"
+            name="stockQuantity"
+            placeholder="Quantidade no estoque"
+            onChange={handleChange}
+            value={formData.stockQuantity}
+            className={inputCssStyles}
+            required
+          />
+
+          <label htmlFor="Rating" className={labelCssStyles}>
+            Rating
+          </label>
+          <input
+            type="number"
+            name="Rating"
+            placeholder="Rating"
+            onChange={handleChange}
+            value={formData.rating}
+            className={inputCssStyles}
+            required
+          />
+
+          <button
+            type="submit"
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          >
+            Adicionar
+          </button>
+          <button
+            onClick={onClose}
+            type="submit"
+            className="ml-2 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700"
+          >
+            Cancelar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default CreateProductModal;
