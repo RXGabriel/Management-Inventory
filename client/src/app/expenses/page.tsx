2cd943033816a6cc78a1a64c729d98330c998050
RXGabriel <gabriel2005rf@gmail.com>
2024-08-28 18:18:49 -0300

    feat: add category/date filters and pie chart visualization

--- client/src/app/expenses/page.tsx
+++ client/src/app/expenses/page.tsx
@@ -4,6 +4,15 @@ import {
   ExpenseByCategorySummary,
   useGetExpensesByCategoryQuery,
 } from "@/state/api";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+import Header from "@/app/(components)/header";
 import React, { useMemo, useState } from "react";
 
 type AggregatedDataItem = {
@@ -62,6 +71,12 @@ const Expenses = () => {
     return Object.values(filtered);
   }, [expenses, selectedCategory, startDate, endDate]);
 
+  const classNames = {
+    label: "block text-sm font-medium text-gray-700",
+    selectInput:
+      "mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md",
+  };
+
   if (isLoading) {
     return <div className="py-4">Carregando...</div>;
   }
@@ -74,7 +89,96 @@ const Expenses = () => {
     );
   }
 
-  return <div>Expenses</div>;
+  return (
+    <div>
+      <div className="mb-5">
+        <Header name="Expenses" />
+        <p className="text-sm text-gray-500">
+          Uma representação visual das despesas ao longo do tempo.
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-between gap-4">
+        <div className="w-full md:w-1/3 bg-white shadow rounded-lg p-6">
+          <h3 className="text-lg font-semibold mb-4">
+            Filtrar por categoria e data
+          </h3>
+          <div className="space-y-4">
+            <div>
+              <label htmlFor="category" className={classNames.label}>
+                Categoria
+              </label>
+              <select
+                id="category"
+                name="category"
+                className={classNames.selectInput}
+                defaultValue="All"
+                onChange={(e) => setSelectedCategory(e.target.value)}
+              >
+                <option>All</option>
+                <option>Office</option>
+                <option>Professional</option>
+                <option>Salaries</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="start-date" className={classNames.label}>
+                Data de início
+              </label>
+              <input
+                type="date"
+                id="start-date"
+                name="start-date"
+                className={classNames.selectInput}
+                onChange={(e) => SetStartDate(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="end-date" className={classNames.label}>
+                Data de término
+              </label>
+              <input
+                type="date"
+                id="end-date"
+                name="end-date"
+                className={classNames.selectInput}
+                onChange={(e) => SetEndDate(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex-grow bg-white shadow rounded-lg p-4 md:p-6">
+          <ResponsiveContainer width="100%" height={400}>
+            <PieChart>
+              <Pie
+                data={aggregatedData}
+                cx="50%"
+                cy="50%"
+                label
+                outerRadius={150}
+                fill="#8884d8"
+                dataKey="amount"
+                onMouseEnter={(_, index) => setActiveIndex(index)}
+              >
+                {aggregatedData.map(
+                  (entry: AggregatedDataItem, index: number) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={
+                        index === activeIndex ? "rgb(29, 78, 216)" : entry.color
+                      }
+                    />
+                  )
+                )}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Expenses;
