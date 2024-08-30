c5153fd1d7897139c76ae2e2c0c8ec6894862cf2
RXGabriel <gabriel2005rf@gmail.com>
2024-08-30 18:11:53 -0300

    feat: add profile image to Navbar component

--- client/src/app/(components)/Navbar.tsx
+++ client/src/app/(components)/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
+import Image from "next/image";
 import React from "react";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
@@ -36,7 +37,7 @@ const Navbar = () => {
             className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
           />
 
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
             <Bell className="text-gray-500" size={20} />
           </div>
         </div>
@@ -63,8 +64,14 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
 
           <div className="flex items-center gap-3 cursor-pointer">
-            <div className="w-9 h-9">image</div>
-            <span className="font-semibold">RXGabriel</span>
+            <Image
+              src="/assets/profile.jpg"
+              alt="Perfil"
+              width={50}
+              height={50}
+              className="rounded-full h-full object-cover"
+            />
+            <span className="font-semibold">Visitante</span>
           </div>
         </div>
 
